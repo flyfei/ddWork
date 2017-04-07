e3cee2e4ae016d32320e830929d9b84997345db4
flyfei <zhaotengfei9@gmail.com>
2017-04-07 14:07:38 +0800

    fix time

--- app/src/main/java/com/tovi/ddwork/Config.java
+++ app/src/main/java/com/tovi/ddwork/Config.java
@@ -7,14 +7,11 @@ import java.util.ArrayList;
  */
 
 public class Config {
-    public static final int ON_WORK_HOUR = 10;
-    public static final int OFF_WORK_HOUR = 19;
-
     public static final boolean AUTO_WORK = true;
-    public static final int AUTO_ON_WORK_HOUR = 9;
-    public static final int AUTO_ON_WORK_MINUTE = 30;
+    public static final int AUTO_ON_WORK_HOUR = 8;
+    public static final int AUTO_ON_WORK_MINUTE = 48;
     public static final int AUTO_OFF_WORK_HOUR = 19;
-    public static final int AUTO_OFF_WORK_MINUTE = 10;
+    public static final int AUTO_OFF_WORK_MINUTE = 0;
 
     public static final boolean AUTO_SEND_EMAIL = true;
     public static final String FROM = "example@sina.com";
